fce9c09ee4de5a4c426823f6c5b58023ed1c8a41
Ben Rogers <benrogers@mac.com>
2015-09-23 08:37:20 -0400

    Document the first couple lines

---
+++ params.sh
@@ -0,0 +1,9 @@
+## This file contains common values for the executable scripts
+
+## Here are values only used within this params.sh file
+DEBUG_LEVEL="low"
+
+
+## Here are values needed by subprocesses as well as the file sourcing this file
+
+export MY_ENVIRONMENT="Apple"
